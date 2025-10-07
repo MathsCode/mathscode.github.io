6f09c1698ff9dbbbf31356d7fa240b68cb9fab8d
Jiaming Xu <jiamingxu@JiamingdeMacBook-Air.local>
2025-10-07 20:57:20 +0800

    init

--- run_server.sh
+++ run_server.sh
@@ -1,1 +1,9 @@
 bundle exec jekyll serve -l
+###
+ # @Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
+ # @Date: 2025-10-07 15:47:57
+ # @LastEditors: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
+ # @LastEditTime: 2025-10-07 20:57:10
+ # @FilePath: /mathscode.github.io/run_server.sh
+ # @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+### 
